6e6c13e1901f84e6eae63b85025f931bcc0c4689
Rick Waldron <waldron.rick@gmail.com>
2016-05-13 18:24:07 -0400

    Use Arrows for app routing
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 const { Board, Led, Servo, Motor, Fn } = require("johnny-five");
 // const Edison = require("edison-io");
