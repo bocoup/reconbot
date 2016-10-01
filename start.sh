1b6f66dfa55949623fa072b1c6e47bad1fbbfa77
Rick Waldron <waldron.rick@gmail.com>
2016-10-01 13:24:34 -0400

    reduce frame dimensions

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 set -e # stop on errors
 set -x # display every command as it's run
 
-/usr/local/bin/mjpg_streamer -i "input_uvc.so -f 15 -r 960x720" -o "output_http.so" &
+/usr/local/bin/mjpg_streamer -i "input_uvc.so -f 15 -r 320x240" -o "output_http.so" &
 npm run start
